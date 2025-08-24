d5b49a1ab6aedc063bc58b7999c3897dd03e0a7b
unknown <istefan-ansari>
2025-08-24 12:59:41 +0530

    Bio-data

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+function openGallery() {
+  document.getElementById("galleryModal").style.display = "block";
+}
+
+function closeGallery() {
+  document.getElementById("galleryModal").style.display = "none";
+}
